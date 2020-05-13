7be1358e0bfaedb7641e2d0c286461de11161f9d
joselitokn2 <joselitokn2@hotmail.com>
2020-05-13 17:32:23 +0200

    primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { Graficas1Component } from './pages/graficas1/graficas1.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { PagesComponent } from './pages/pages.component';
+import { RegistrerComponent } from './login/registrer.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {           path: '',
+              component: PagesComponent,
+              children: [
+                {path: 'dashboard', component: DashboardComponent},
+                {path: 'progress', component: LoginComponent},
+                {path: 'graficas1', component: Graficas1Component},
+                {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+              ]},
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+  {path: 'login', component: LoginComponent},
+  {path: 'registrer', component: RegistrerComponent},
+  {path: '**', component: NopagefoundComponent}
+];
+
+
+  export const APP_ROUTES = RouterModule.forRoot( routes, { useHash: true } );
+
---
+++ src/app/login/registrer.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-registrer',
+  templateUrl: './registrer.component.html',
+  styles: [
+  ]
+})
+export class RegistrerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
