10fabf75da5402dd1b8eb23c8087b174de8b2177
joselitokn2 <joselitokn2@hotmail.com>
2020-07-14 12:05:06 +0200

    componente mensajes

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { UsuarioService } from 'src/app/services/service.index';
-import { Usuario } from 'src/app/models/usuario.model';
 import { Router } from '@angular/router';
+import { Usuario } from 'src/app/models/usuario.model';
+import { UsuarioService } from 'src/app/services/service.index';
 
 @Component({
   selector: 'app-header',
@@ -10,12 +10,24 @@ import { Router } from '@angular/router';
 })
 export class HeaderComponent implements OnInit {
   usuario: Usuario;
+  usuarios: Usuario[] = [];
+  puntero = 0;
+  totalUsuarios: number;
+
   constructor(public usuarioService: UsuarioService, public router: Router) {}
 
   ngOnInit(): void {
     this.usuario = this.usuarioService.usuario;
+    this.cargarUsuarios();
   }
   buscar(busqueda: string) {
     this.router.navigate(['/buscador/' + busqueda]);
+  }
+  cargarUsuarios() {
+    this.usuarioService.cargarUsuarios(this.puntero).subscribe((resp: any) => {
+      // console.log(resp);
+      this.totalUsuarios = resp.total;
+      this.usuarios = resp.usuarios;
+    });
   }
 }
